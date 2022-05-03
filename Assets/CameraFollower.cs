04ae764d42c6409631d488d75599aa5fde4bf32f
Antonio José <delatorreperezantonio@gmail.com>
2022-05-03 13:15:28 +0200

    CameraFollower
    Le falta la rotacion por arreglar

--- Assets/CameraFollower.cs
+++ Assets/CameraFollower.cs
@@ -4,11 +4,15 @@ public class CameraFollower : MonoBehaviour
 {
     public Transform objetivo;
 
-    public float smoothSpeed = 0.125f;
+    public float velocidadSuavizada = 0.125f;
     public Vector3 offset;
 
-    private void LateUpdate()
+    private void FixedUpdate()
     {
-        transform.position = objetivo.position + offset;
+        Vector3 posicionDeseada = objetivo.position + offset;
+        Vector3 posicionSuavizada = Vector3.Lerp(transform.position, posicionDeseada, velocidadSuavizada);
+        transform.position = posicionSuavizada;
+        transform.rotation = objetivo.rotation;
+        transform.LookAt(objetivo);
     }
 }
