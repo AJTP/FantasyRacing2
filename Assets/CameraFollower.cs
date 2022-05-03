ec8cda806bd0c71f6ca71ae01ca148074c43fb3f
Antonio <ajdltpsg6@gmail.com>
2022-05-04 00:28:34 +0200

    sdf

--- Assets/CameraFollower.cs
+++ Assets/CameraFollower.cs
@@ -3,16 +3,14 @@ using UnityEngine;
 public class CameraFollower : MonoBehaviour
 {
     public Transform objetivo;
+    public Transform puntoCamara;
 
     public float velocidadSuavizada = 0.125f;
-    public Vector3 offset;
-
+  
     private void FixedUpdate()
     {
-        Vector3 posicionDeseada = objetivo.position + offset;
-        Vector3 posicionSuavizada = Vector3.Lerp(transform.position, posicionDeseada, velocidadSuavizada);
-        transform.position = posicionSuavizada;
-        transform.rotation = objetivo.rotation;
-        transform.LookAt(objetivo);
+        transform.position = new Vector3(puntoCamara.transform.position.x, puntoCamara.transform.position.y+1, puntoCamara.transform.position.z);
+        transform.rotation = new Quaternion(0f, puntoCamara.rotation.eulerAngles.y,0f, 0f);
+        //transform.LookAt(objetivo);
     }
 }
