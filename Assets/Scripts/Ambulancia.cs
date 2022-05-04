ff0c2b21769cd10bff24acf4a1bce59dac70274c
Antonio <ajdltpsg6@gmail.com>
2022-05-04 20:58:47 +0200

    Script Ambulancia
    Primer Script control Vehiculo concreto y Creacion Prefabs con modelos

--- Assets/Scripts/Ambulancia.cs
+++ Assets/Scripts/Ambulancia.cs
@@ -1,44 +1,135 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ambulancia : Coche
 {
+    private GameObject canvas;
+
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        esfera.transform.parent = null;
+        //CARGAR COOLDOWNS DESDE ALGUN SITIO
+        coolDowns[0] = 4;
+        coolDowns[1] = 8;
+        coolDowns[2] = 12;
+        coolDowns[3] = 16;
+        for(int i=0;i<timerCD.Length;i++) {
+            timerCD[i] = coolDowns[i];
+        }
+        //CARGAR IMAGENES
         
     }
 
-    //USA LAS SIRENAS Y AUMENTA SU VELOCIDAD
-    private void Habilidad1(){
-        //Activar Sirenas durante la duración de la habilidad
-
-        //Recibir Boost de velocidad
-        RecibirBoost(1f);
+    void Update()
+    {
+        RecogerInputMovimientoBasico();
+        int habilidad = RecogerInputHabilidades();
+        if (habilidad != 5) {
+            LanzarHabilidad(habilidad);
+        }
+        for (int i = 0; i < isCD.Length; i++)
+        {
+            if (isCD[i] == true)
+            {
+                ReducirCoolDown(i);
+            }
+        }
     }
-    // Deja en el suelo un charco de sangre que resbala
-    private void Habilidad2(){
+
+    private void FixedUpdate()
+    {
+        AplicarVelocidad();
+    }
+
+    private int RecogerInputHabilidades()
+    {
+        if (Input.GetKeyDown("u"))
+        {
+            return 0;
+        }
+        else if (Input.GetKeyDown("i"))
+        {
+            return 1;
+        }
+        else if (Input.GetKeyDown("o"))
+        {
+            return 2;
+        }
+        else if (Input.GetKeyDown("p"))
+        {
+            return 3;
+        }
+
+        return 5;
+    }
+
+    private void ReducirCoolDown(int i) {
+            if (timerCD[i] < coolDowns[i])
+            {
+                imagenes[i].color = Color.red;
+                timerCD[i] += Time.deltaTime;
+                imagenes[i].fillAmount = timerCD[i] / coolDowns[i];
+            }
+            else if(timerCD[i]>= coolDowns[i]) {
+                imagenes[i].color = Color.cyan;
+                timerCD[i] = coolDowns[i];
+                imagenes[i].fillAmount = 1;
+                isCD[i] = false;
+            }           
+    }
+
+    private void LanzarHabilidad(int i) {
+        if (timerCD[i] >= coolDowns[i])
+        {
+            isCD[i] = true;
+            timerCD[i] = 0;
+            switch (i) {
+                case 0: 
+                    Habilidad0();
+                    break;
+                case 1:
+                    Habilidad1();
+                    break;
+                case 2:
+                    Habilidad2();
+                    break;
+                case 3:
+                    Habilidad3();
+                    break;
+            }
+        }
+    }
+
+    private void Habilidad0(){
+        //USA LAS SIRENAS Y AUMENTA SU VELOCIDAD
+        print("HABILIDA 0 LANZADA");
+        //Activar Sirenas durante la duración de la habilidad
+        //Recibir Boost de velocidad
+        RecibirBoost(20000f);
+    }
+
+    private void Habilidad1(){
+        // Deja en el suelo un charco de sangre que resbala
+        print("HABILIDA 1 LANZADA");
         //Soltar prefab
         SoltarPrefab();
     }
-    //REGENERA SU VIDA UN 30%
-    private void Habilidad3(){
-        // Sumar un % de vida
-        vida+=vidaMaxima/3;
-        if(vida>vidaMaxima)
-            vida = vidaMaxima;
 
+    private void Habilidad2(){
+        //REGENERA SU VIDA UN 30%
+        print("HABILIDA 2 LANZADA");
+        // Sumar un % de vida
+        vida += vidaMaxima / 3;
+        if (vida > vidaMaxima)
+            vida = vidaMaxima;
         //Animacion suma vida
     }
 
-    //ELECTROCUTA AL RETO DE JUGADORES Y LOS DEJA INMOVILIZADOS
-    private void Habilidad4(){
+    private void Habilidad3(){
+        //ELECTROCUTA AL RETO DE JUGADORES Y LOS DEJA INMOVILIZADOS
+        print("HABILIDA 3 LANZADA");
         //Animación electricidad
         //Aplica Stun al resto de jugadores
         //Animación electricidad en el jugador que recibe el stun
--- Assets/Scripts/Coche.cs
+++ Assets/Scripts/Coche.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Coche : MonoBehaviour
 {
     public Rigidbody esfera;
-    public float aceleracion=8f, marchaAtras=4f, velocidadMaxima=50f, fuerzaGiro=180f,gravedad=10f,dragEnSuelo = 3f;
-    
+    public float aceleracion = 8f, marchaAtras = 4f, velocidadMaxima = 50f, fuerzaGiro = 180f, gravedad = 10f, dragEnSuelo = 3f;
+
     public LayerMask suelo;
     public float longitudRayoSuelo = 0.5f;
     public Transform puntoRayoSuelo;
@@ -18,21 +19,20 @@ public class Coche : MonoBehaviour
     private float velocidadInput, giroInput;
     private bool tocandoSuelo;
     #region INFORMACIÓN JUGADOR
+    public int posicion;
     private int idJugador;
     private string nombreJugador;
     #endregion
     #region STATS COCHE
-    //COOLDOWN
-    public float cd1,cd2,cd3,cd4;
+    //COOLDOWNS
+    protected bool[] isCD = new bool[4];
+    protected float[] coolDowns = new float[4];
+    protected float[] timerCD= new float[4];
+    public Image[] imagenes = new Image[4];
+
     public int vida,vidaMaxima;
     #endregion
-    private void Start()
-    {
-        esfera.transform.parent = null;
-    }
-
-    private void Update()
-    {
+    public void RecogerInputMovimientoBasico() {
         velocidadInput = 0f;
         //RECOGIDA ACELERACION
         if (Input.GetAxis("Vertical") > 0)
@@ -40,7 +40,8 @@ public class Coche : MonoBehaviour
             //ACELERA HACIA DELANTE
             velocidadInput = Input.GetAxis("Vertical") * aceleracion * multiplicador;
         }
-        else if (Input.GetAxis("Vertical") < 0) {
+        else if (Input.GetAxis("Vertical") < 0)
+        {
             //ACELERA MARCHA ATRAS
             velocidadInput = Input.GetAxis("Vertical") * marchaAtras * multiplicador;
         }
@@ -49,29 +50,29 @@ public class Coche : MonoBehaviour
         //RECOGIDA DIRECCION
         giroInput = Input.GetAxis("Horizontal");
 
-        if (tocandoSuelo) {
+        if (tocandoSuelo)
+        {
             transform.rotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, giroInput * fuerzaGiro * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
         }
 
         //ROTACION RUEDAS #ESTETICO
-        ruedaIzquierda.localRotation = Quaternion.Euler(ruedaIzquierda.localRotation.eulerAngles.x,(giroInput * maximaRotacionRuedas)-180,ruedaIzquierda.localRotation.eulerAngles.z);
-        ruedaDerecha.localRotation = Quaternion.Euler(ruedaDerecha.localRotation.eulerAngles.x,giroInput * maximaRotacionRuedas, ruedaDerecha.localRotation.eulerAngles.z);
-        
+        ruedaIzquierda.localRotation = Quaternion.Euler(ruedaIzquierda.localRotation.eulerAngles.x, (giroInput * maximaRotacionRuedas) - 180, ruedaIzquierda.localRotation.eulerAngles.z);
+        ruedaDerecha.localRotation = Quaternion.Euler(ruedaDerecha.localRotation.eulerAngles.x, giroInput * maximaRotacionRuedas, ruedaDerecha.localRotation.eulerAngles.z);
+
         transform.position = esfera.position;
     }
 
-    private void FixedUpdate()
-    {
+    public void AplicarVelocidad() {
         tocandoSuelo = false;
         RaycastHit hit;
 
         //COMPROBACION TOCANDO SUELO
-        if (Physics.Raycast(puntoRayoSuelo.position, -transform.up, out hit, longitudRayoSuelo, suelo)) {
+        if (Physics.Raycast(puntoRayoSuelo.position, -transform.up, out hit, longitudRayoSuelo, suelo))
+        {
             tocandoSuelo = true;
 
             transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
         }
-
 
         if (tocandoSuelo)
         {
@@ -81,21 +82,21 @@ public class Coche : MonoBehaviour
                 //SUMAR VELOCIDAD
                 esfera.AddForce(transform.forward * velocidadInput);
             }
-        }
-        else {
+        }else{
             //ESTA EN EL AIRE
             esfera.drag = 0.1f;
             esfera.AddForce(Vector3.up * -gravedad * 100f);
         }
-
     }
 
-    public void RecibirBoost(float cantidad){
 
+    public void RecibirBoost(float cantidad){
+        //NO FUNCIONA
+        //esfera.AddForce(transform.forward * cantidad);
     }
 
     public void RecibirStun(float tiempo){
-
+        
     }
 
     public void SoltarPrefab(){
