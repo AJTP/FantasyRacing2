67f66be9c44d34bee884bb824fa2d37240c8caf4
Antonio José <83008558+AJTP@users.noreply.github.com>
2022-05-05 12:10:52 +0200

    CambiosOrden

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject[] puntosControl;
+    void Start()
+    {
+        //GameObject.Find("PuntosControl").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
