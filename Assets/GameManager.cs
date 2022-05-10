861e2ecf7249e5d4a691e72ae994d69ad784ee11
Antonio José <83008558+AJTP@users.noreply.github.com>
2022-05-10 17:07:31 +0200

    GameManagerModificado

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private GameObject[] puntosControl;
+    GameObject puntosControl;
+    List<GameObject> puntos = new List<GameObject>();
     void Start()
     {
-        //GameObject.Find("PuntosControl").transform;
-    }
+        //PUNTOS DE CONTROL CARGADOS
+        puntosControl = GameObject.Find("PuntosControl");
+        for(int i=0;i<puntosControl.transform.childCount;i++){
+            puntos.Add(puntosControl.transform.GetChild(i).gameObject);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
         
     }
 }
