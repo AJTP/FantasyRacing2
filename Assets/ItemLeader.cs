4d53f2e03f96bf175014daf77c7d7bbf5683ccf0
Antonio <ajdltpsg6@gmail.com>
2022-06-03 18:42:10 +0200

    Leaderboard Item Minutos Segundos

--- Assets/ItemLeader.cs
+++ Assets/ItemLeader.cs
@@ -9,6 +9,13 @@ public class ItemLeader : MonoBehaviour
 
     public void SetDatosItem(string _nombre,int _tiempo) {
         nombre.text = _nombre;
-        tiempo.text = ""+_tiempo;
+        tiempo.text = SecsToMin(_tiempo);
+    }
+
+    public string SecsToMin(int segundos) {
+        int min;
+        min = segundos / 60;
+        segundos = segundos - min * 60;
+        return string.Format("{0:D2}:{1:D2}",min,segundos);
     }
 }
